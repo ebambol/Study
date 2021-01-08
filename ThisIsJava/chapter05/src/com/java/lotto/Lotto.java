7afd35e5bf5c928b0cfb66fbad5f9cbef976b18a
ebambol <pale0865@gmail.com>
2021-01-08 15:52:03 +0900

    java

--- ThisIsJava/chapter05/src/com/java/lotto/Lotto.java
+++ ThisIsJava/chapter05/src/com/java/lotto/Lotto.java
@@ -9,19 +9,15 @@ public class Lotto {
 		int[] arr = new int[6];
 		for(int i=0; i<6; i++) {
 			arr[i] = (int)(Math.random()*45)+1;
-			for(int j=0; j<6; j++) {
-				if(arr[i] == arr[j]) {
-					continue;
+			for(int j=0; j<i; j++) {
+				if(arr [i] == arr[j]) {
+					i--;
+					break;
 				}
-				System.out.println(arr[i]);
 			}
 		}
-//		System.out.println(arr);
-//		while (true) {
-//			int random = (int)(Math.random()*45)+1;
-//		}
-		
-		
+		for(int i=0; i<6; i++) {
+			System.out.println(arr[i]);
+		}
 	}
-
 }
