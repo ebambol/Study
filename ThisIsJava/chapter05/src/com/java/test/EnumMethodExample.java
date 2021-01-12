a0493ef492edec8e1508961d78cbed25c0654d2b
ebambol <pale0865@gmail.com>
2021-01-12 14:30:25 +0900

    2021-01-12

--- ThisIsJava/chapter05/src/com/java/test/EnumMethodExample.java
+++ ThisIsJava/chapter05/src/com/java/test/EnumMethodExample.java
@@ -3,6 +3,18 @@ package com.java.test;
 public class EnumMethodExample {
 
 	public static void main(String[] args) {
+		/*
+		 * public enum Week {
+				MONDAY,		//0
+				TUESDAY,	//1
+				WEDNESDAY,	//2
+				THURDAY,	//3
+				FRIDAY,		//4
+				SATURDAY,	//5
+				SUNDAY		//6
+			}
+		 */
+		
 		//name() 메소드	: 열거 객체의 문자열 리턴
 		Week today = Week.SUNDAY;
 		String name = today.name();
@@ -12,7 +24,7 @@ public class EnumMethodExample {
 		int ordinal = today.ordinal();
 		System.out.println(ordinal);
 		
-		//compareTo() 메소드	:
+		//compareTo() 메소드	: 열거 객체를 비교해서 순번 차이를 리턴
 		Week day1 = Week.MONDAY;
 		Week day2 = Week.WEDNESDAY;
 		int result1 = day1.compareTo(day2);
@@ -20,7 +32,7 @@ public class EnumMethodExample {
 		System.out.println(result1);
 		System.out.println(result2);
 		
-		//valueOf() 메소드
+		//valueOf() 메소드 : 주어진 문자열의 열거 객체를 리턴
 		if(args.length == 1) {
 			String strDay = args[0];
 			Week weekDay = Week.valueOf(strDay);
@@ -31,7 +43,7 @@ public class EnumMethodExample {
 			}
 		}
 		
-		//values() 메소드
+		//values() 메소드 : 모든 열거객체들을 배열로 리턴
 		Week[] days = Week.values();
 		for(Week day : days) {
 			System.out.println(day);
