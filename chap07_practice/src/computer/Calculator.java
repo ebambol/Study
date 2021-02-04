9a6bf9ddf0b9567df77c23dcba9e3771d336e5a2
ebambol <73654909+ebambol@users.noreply.github.com>
2021-02-04 14:50:23 +0900

    Delete chap07_practice directory

--- chap07_practice/src/computer/Calculator.java
+++
@@ -1,8 +0,0 @@
-package computer;
-
-public class Calculator {
-	double areaCircle(double r) {
-		System.out.println("Calculator °´Ã¼");
-		return 3.14159 * r * r;
-	}
-}
--- chap07_practice/src/protected1/C.java
+++
@@ -1,5 +0,0 @@
-package protected1;
-
-public class C {
-
-}
